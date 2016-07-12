537178cc966113c47665b1aaeb45a6532e2d4ea1
mohayonao <mohayonao@gmail.com>
2016-07-12 14:42:21 +0900

    refine synth unit #1
      - SCUnitOut

--- test/unit/SCUnitOut.js
+++ test/unit/SCUnitOut.js
@@ -14,13 +14,13 @@ test("a", () => {
     paramIndices: {},
     units: [
       [ "DC" , 2, 0, [ [ -1, 0 ]                     ], [ 2 ] ],
-      [ "DC" , 1, 0, [ [ -1, 0 ]                     ], [ 1 ] ],
+      [ "DC" , 2, 0, [ [ -1, 0 ]                     ], [ 2 ] ],
       [ "Out", 2, 0, [ [ -1, 0 ], [ 0, 0 ], [ 1, 0 ] ], [   ] ]
     ]
   };
   const synth = context.createSynth(synthdef);
   const noise0 = Float32Array.from({ length: 8 }, () => Math.random());
-  const noise1 = Float32Array.from({ length: 1 }, () => Math.random());
+  const noise1 = Float32Array.from({ length: 8 }, () => Math.random());
 
   synth.unitList[0].outputs[0].set(noise0);
   synth.unitList[1].outputs[0].set(noise1);
@@ -29,9 +29,11 @@ test("a", () => {
   context.process();
 
   assert.deepEqual(context.audioBuses[0], noise0);
-  assert.deepEqual(context.audioBuses[1], [ noise1[0], 0, 0, 0, 0, 0, 0, 0 ]);
+  assert.deepEqual(context.audioBuses[1], noise1);
   assert.deepEqual(context.outputs[0], noise0);
-  assert.deepEqual(context.outputs[1], [ noise1[0], 0, 0, 0, 0, 0, 0, 0 ]);
+  assert.deepEqual(context.outputs[1], noise1);
+
+  synth.close();
 });
 
 test("k", () => {
@@ -60,4 +62,6 @@ test("k", () => {
   assert.deepEqual(context.controlBuses[1], [ noise1[0] ]);
   assert.deepEqual(context.outputs[0], [ 0, 0, 0, 0, 0, 0, 0, 0 ]);
   assert.deepEqual(context.outputs[1], [ 0, 0, 0, 0, 0, 0, 0, 0 ]);
+
+  synth.close();
 });
