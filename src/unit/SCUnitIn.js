2abb18d1453ff381d73d2b3a8f1e46357d7d2b2b
mohayonao <mohayonao@gmail.com>
2016-07-19 22:12:11 +0900

    refine synth unit #1
      - SCUnitIn

--- src/unit/SCUnitIn.js
+++ src/unit/SCUnitIn.js
@@ -1,10 +1,15 @@
 "use strict";
+
+const assert = require("assert");
 const C = require("../Constants");
 const SCUnit = require("../SCUnit");
 const SCUnitRepository = require("../SCUnitRepository");
+
 const dspProcess = {};
+
 class SCUnitIn extends SCUnit {
   initialize() {
+    assert(this.inputs.length === 1);
     if (this.calcRate === C.RATE_AUDIO) {
       this.dspProcess = dspProcess["a"];
       this._buses = this.context.audioBuses;
@@ -14,11 +19,27 @@ class SCUnitIn extends SCUnit {
     }
   }
 }
+
 dspProcess["a"] = function () {
-  this.outputs[0].set(this._buses[this.inputs[0][0] | 0]);
+  const outputs = this.outputs;
+  const buses = this._buses;
+  const firstBusChannel = this.inputs[0][0]|0;
+
+  for (let ch = 0, chmax = outputs.length; ch < chmax; ch++) {
+    outputs[ch].set(buses[firstBusChannel + ch]);
+  }
 };
+
 dspProcess["k"] = function () {
-  this.outputs[0][0] = this._buses[this.inputs[0][0] | 0][0];
+  const outputs = this.outputs;
+  const buses = this._buses;
+  const firstBusChannel = this.inputs[0][0]|0;
+
+  for (let ch = 0, chmax = outputs.length; ch < chmax; ch++) {
+    outputs[ch][0] = buses[firstBusChannel + ch][0];
+  }
 };
+
 SCUnitRepository.registerSCUnitClass("In", SCUnitIn);
+
 module.exports = SCUnitIn;
